ad1e0cf7d8fd7ebd72c585ecccd39588afb4ceb8
ruby -v <Jbssantias@hotmail.com>
2019-08-23 14:48:03 +1000

    playing with news API

--- newstube/routes/index.js
+++ newstube/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+const express = require('express');
+const processor = require('../scripts/process_news');
+const NewsAPI = require('newsapi');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//News API key
+const newsapi = new NewsAPI('bf12e04bc8c446c0862f63962dff0bad');
 
-module.exports = router;
+router.get('/', function(req, res, next) {
+	const title = 'Top Articles from BuzzFeed'
+	// Retrieve the top headlines from Buzzfeed
+	processor.getTopArticles().then((response) => {
+		// Filter the responses and create objects to be displayed
+        let articles = processor.buildArticles(response);
+        
+        //render the page
+        res.render('index', {title: title});
+
+		// Apply tones to the objects
+	}).catch((err) => {
+	  res.render('error', { message: "An Error Occured Fetching Articles", error: err });
+	});
+});
