8f03b842b2eaa16b8d3d9ccd4ef72eb4a68efd6b
ruby -v <Jbssantias@hotmail.com>
2019-09-04 17:46:44 +1000

    content extraction

---
+++ newstube/scripts/extractContent.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const AYLIENTextAPI = require('aylien_textapi')
+
+// Text Analysis API Key
+const textapi_key = process.env.ANALYSIS_KEY;
+const textapi_id = process.env.ANALYSIS_ID;
+
+
+function extractNewsContent(link){
+	// var content;
+	var textapi = new AYLIENTextAPI({
+		application_id: textapi_id,
+		application_key: textapi_key
+	});
+
+	textapi.extract({url: link, best_image: false}, function(error, response) {
+		content = response.article;
+		return content;
+
+	});
+	return content;
+	// console.log(content);
+}
+
+module.exports.extractNewsContent = extractNewsContent;
