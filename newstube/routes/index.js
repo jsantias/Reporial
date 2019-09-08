7b6cdc29d9538bc88f485bc84ce8646b81504f40
ruby -v <Jbssantias@hotmail.com>
2019-09-08 15:12:29 +1000

    analytics returns data for each article and displays graph

--- newstube/routes/index.js
+++ newstube/routes/index.js
@@ -3,7 +3,6 @@ const processNews = require('../scripts/processNews');
 const processAnalysis = require('../scripts/processAnalysis');
 const extractContent = require('../scripts/extractContent');
 
-
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -15,42 +14,16 @@ router.post('/', function(req, res, next) {
   let query = req.body.query;
   var title = "Results for '" + query + "'";
   processNews.getTopArticles(query).then((response) => {
-    // console.log(response);
     let articles = processNews.buildArticles(response);
-    // console.log(articles);
-    extractContent.extractNewsContent(articles).then((response) => {
-      console.log(response);
-      res.render('index', {title: title, data: response});
+    processAnalysis.buildAnalytics(articles).then((response) => {
 
-    }).catch((err) => {
-      console.log("error extracting content");
-    })
-    // console.log(content);
-    // let text_analysis = processAnalysis.buildAnalytics(articles);
-    // console.log(text_analysis);
-    // res.render('index', {title: title, data: text_analysis});
-
-    // processAnalysis.buildAnalytics(articles, function(text_analysis){
-    //   console.log(text_analysis);
-    //   res.render('index', {title: title, data: response.data, analytics: text_analysis});
-    // }).catch((err) => {
-    //   res.render('error', { message: "An Error Occured analysing content", error: err })
-    // })
+      res.render('index', {title: title, reports: articles, analytics: response});
+    }).then((err) => {
+      console.log(err);
+    });
   }).catch((err) => {
     res.render('error', { message: "An Error Occured Extracting Content", error: err });
   });
-  // processAnalysis.sentimentAnalysis(query).then((response) => {
-  //   res.render('result', {title: title, analyse: response});
-  //   // console.log(response);
-
-  // }).catch((err) => {
-  //   res.render('error', { message: "An Error Occured Extracting Content", error: err });
-  // });
-    // // res.render('layout');
-    // // console.log(query);
-    // var result = extractContent.extractArticle(query);
-    // console.log(result);
-  
 });
 
 module.exports = router;
--- newstube/scripts/processAnalysis.js
+++ newstube/scripts/processAnalysis.js
@@ -1,39 +1,39 @@
 require('dotenv').config();
-const extractContent = require('./extractContent');
 
 var NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js');
 
 const ibm_key = process.env.IBM_KEY;
 
-async function sentimentAnalysis(query) {
-    return new Promise((resolve, reject) => {
-		var nlu = new NaturalLanguageUnderstandingV1({
-			iam_apikey: ibm_key,
-			version: '2018-04-05',
-			url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
-		  });
-		   
-		  nlu.analyze(
-			{
-			  url: query, // Buffer or String
-			  features: {
-				concepts: {},
-				categories: {},
-				// keywords: {},
-				sentiment: {},
-				emotion: {},
-				semantic_roles: {}
-			  }
-			})
-			.then(result => {
-				var re = extractContent.extractNewsContent(query);
-				// console.log(result);
-				resolve(result);
-			})
-			.catch(err => {
-			  console.log('error:', err);
-			});
-    });
+var nlu = new NaturalLanguageUnderstandingV1({
+	iam_apikey: ibm_key,
+	version: '2018-04-05',
+	url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
+
+async function buildAnalytics(articles) {
+	// console.log(articles)
+	const articlesPromises = articles.map(async (value) => {
+		//count++;
+		var params = {
+			'url': value.url, // Buffer or String
+			'features': {
+				'concepts': {},
+				'sentiment': {},
+				'emotion': {},
+			}
+		}
+		try {
+			return await nlu.analyze(params);
+		} catch(e) {
+			return null;
+		}
+
+	});
+	let result = await Promise.all(articlesPromises)
+	console.log('before filter', result.length)
+	result = result.filter(r => r);
+	console.log('filtered', result.length)
+	return result;
 }
 
-module.exports.sentimentAnalysis = sentimentAnalysis;
+module.exports.buildAnalytics = buildAnalytics;
