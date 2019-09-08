2264e7e107b4306b967e8775aacc8e59637ea202
ruby -v <Jbssantias@hotmail.com>
2019-09-08 11:03:22 +1000

    promise fixed for extractcontent

--- newstube/routes/index.js
+++ newstube/routes/index.js
@@ -15,7 +15,27 @@ router.post('/', function(req, res, next) {
   let query = req.body.query;
   var title = "Results for '" + query + "'";
   processNews.getTopArticles(query).then((response) => {
-    res.render('index', {title: title, articles: response.articles});
+    // console.log(response);
+    let articles = processNews.buildArticles(response);
+    // console.log(articles);
+    extractContent.extractNewsContent(articles).then((response) => {
+      console.log(response);
+      res.render('index', {title: title, data: response});
+
+    }).catch((err) => {
+      console.log("error extracting content");
+    })
+    // console.log(content);
+    // let text_analysis = processAnalysis.buildAnalytics(articles);
+    // console.log(text_analysis);
+    // res.render('index', {title: title, data: text_analysis});
+
+    // processAnalysis.buildAnalytics(articles, function(text_analysis){
+    //   console.log(text_analysis);
+    //   res.render('index', {title: title, data: response.data, analytics: text_analysis});
+    // }).catch((err) => {
+    //   res.render('error', { message: "An Error Occured analysing content", error: err })
+    // })
   }).catch((err) => {
     res.render('error', { message: "An Error Occured Extracting Content", error: err });
   });
--- newstube/scripts/extractContent.js
+++ newstube/scripts/extractContent.js
@@ -1,45 +1,68 @@
-require('dotenv').config();
-const AYLIENTextAPI = require('aylien_textapi')
+const {
+	extract 
+  } = require('article-parser');
+  const stripHtml = require("string-strip-html");
+   
+async function extractNewsContent(articles){
+	return new Promise((resolve, reject) => {
+		const urlArray = [];
+		let finalArray = [];
+		let finalValue = {"content": {}};
+		// console.log(articles);
+		articles.forEach(element => {
+			urlArray.push(element.url);
+		});
+		// console.log(urlArray);
 
-// Text Analysis API Key
-const textapi_key = process.env.ANALYSIS_KEY;
-const textapi_id = process.env.ANALYSIS_ID;
-
-
-async function extractNewsContent(link){
-	// var content;
-	// return new Promise((resolve, reject) => {
-	// 	var textapi = new AYLIENTextAPI({
-	// 		application_id: textapi_id,
-	// 		application_key: textapi_key
-	// 	});
-
-	// 	textapi.extract({
-	// 		url: link, 
-	// 		best_image: false
-	// 	})
-	// 	.then(response => {
-	// 		resolve(result);
-	// 	})
-	// 	.catch(err => {
-	// 		console.log('error: ', err);
-	// 	});
+		urlArray.forEach((value) => {
+			finalArray.push(extract(value).then((result) => {
+				finalValue.content = result.content;
+				return finalValue;
+			}));
+		});
+		const resolvedFinal = Promise.all(finalArray);
+		resolve(resolvedFinal);
+	});
+	// const urlArray = [];
+	// let finalArray = [];
+	// let finalValue = {"content": {}};
+	// // console.log(articles);
+	// articles.forEach(element => {
+	// 	urlArray.push(element.url);
 	// });
+	// // console.log(urlArray);
+
+	// urlArray.forEach((value) => {
+	// 	finalArray.push(extract(value).then((result) => {
+	// 		finalValue.content = result.content;
+	// 		return finalValue;
+	// 	}));
+	// });
+	// const resolvedFinal = await Promise.all(finalArray);
+	// return resolvedFinal;
+	// console.log(resolvedFinal);
+	// for (let i = 0; i < finalValue.length; i++){
+	// 	resolvedFinal[i].content = stripHtml(resolvedFinal[i].content);
+	// 	// console.log(resolvedFinal);
+	// }
+	// resolve(resolvedFinal);
 	
 	
-	var textapi = new AYLIENTextAPI({
-		application_id: textapi_id,
-		application_key: textapi_key
-	});
-
-	let content2 = await textapi.extract({url: link, best_image: false}, function(error, response) {
-		content = response.article;
-		// console.log(response);
-		return content;
-
-	});
-	console.log(content2);
-	return content2;
+	// for (let i = 0; i < articles.length; i++){
+	// 	let url = articles[i].url;
+   
+	// 	extract(url).then((article) => {
+	// 		var content = stripHtml(article.content);
+	// 		articles[i].content = content;
+	// 		// console.log(stripHtml(content));
+	// 		// return articles[i];
+			
+	// 	}).catch((err) => {
+	// 	  console.log(err);
+	// 	});
+	// }
+	// console.log(articles);
+	// return articles;
 }
 
 module.exports.extractNewsContent = extractNewsContent;
